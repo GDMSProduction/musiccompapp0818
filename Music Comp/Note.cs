c1e8ce7b6a76dbb7ab3ecbf0becfdb9a2bce3ad9
Jacob Pendleton <JDPendleton@student.fullsail.edu>
2018-08-01 13:23:05 -0400

    added Note class

---
+++ Music Comp/Note.cs
@@ -0,0 +1,30 @@
+﻿namespace Music_Comp
+{
+    class Note
+    {
+        Pitch pitch;
+        Accidental accidental;
+        Durration durration;
+        byte octave = 4;
+
+        public Note(Pitch p, Accidental a, Durration d, byte o)
+        {
+            pitch = p;
+            accidental = a;
+            durration = d;
+            octave = o;
+        }
+        public Pitch getPitch()
+        {
+            return pitch;
+        }
+        public Accidental getAccidental()
+        {
+            return accidental;
+        }
+        public Durration getDurration()
+        {
+            return durration;
+        }
+    }
+}
