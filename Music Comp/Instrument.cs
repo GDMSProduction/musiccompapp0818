cc72e05b1a98ced85d4ef3b8684050b01a047848
Jacob Pendleton <JDPendleton@student.fullsail.edu>
2018-08-01 13:28:16 -0400

    added Instrument class

---
+++ Music Comp/Instrument.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Music_Comp
+{
+    class Instrument
+    {
+        public List<Staff> staves = new List<Staff>();
+
+        public Instrument(int numberOfStaves, Clef c, Key k, Time t)
+        {
+            for (int i = 0; i < numberOfStaves; i++)
+            {
+                Staff s = new Staff(i, c, k, t);
+                staves.Add(s);
+            }
+        }
+        public Staff getStaff(int i)
+        {
+            return staves[i];
+        }
+    }
+}
