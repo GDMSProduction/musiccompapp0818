6125a63b88e689399b7abbaada69c49fb16290e6
Jacob Pendleton <JDPendleton@student.fullsail.edu>
2018-08-01 13:24:24 -0400

    added Measure class

---
+++ Music Comp/Measure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Music_Comp
+{
+    class Measure
+    {
+        List<Note> notes = new List<Note>();
+        int length = (Console.LargestWindowWidth - 3);
+        int height = 15;
+        int staffNumber;
+        int cursor;
+
+        public Measure(int sn, int c)
+        {
+            staffNumber = sn;
+            cursor = c;
+            drawBarLine();
+        }
+        public Note getNote(int i)
+        {
+            return notes[i];
+        }
+        public void drawBarLine()
+        {
+
+        }
+    }
+}
